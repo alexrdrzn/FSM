4667f68314e7b3b20a21cd766f7bf76bfd681e3b
alexrdrzn <alex.rdrz.n@gmail.com>
2021-07-28 12:56:57 -0500

    Primer commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
+#include <Ultrasonic.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define FORWARD 100
+#define TURN 200
+#define BACKWARD 210
+#define LEFT 220
+#define RIGHT 230
+#define ERROR 404
+
+const uint8_t upperTrigger 	= 0;
+const uint8_t upperEcho 	= 4;
+const uint8_t lowerTrigger 	= 16;
+const uint8_t lowerEcho 	= 17;
+const uint8_t rightTrigger 	= 5;
+const uint8_t rightEcho 	= 18;
+
+void setup()
+{
+	// declarar los sensores
+	Ultrasonic upperSensor(upperTrigger, upperEcho);
+	Ultrasonic lowerSensor(lowerTrigger, lowerEcho);
+	Ultrasonic rightSensor(rightTrigger, rightEcho);
+	
+	// declarar los motores
+	
+	// declarar los pines auxiliares
+	
+	//inicializar el puerto serial
+	Serial.begin(115200);
+	
+	//inicializar wifi?
+	//correr la webapage de configuracion?
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	// iniciar la fsm
+
 }
